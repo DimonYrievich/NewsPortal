70dbcfb95c7d74adc8ac663d46d31e8cdfa0cb4e
DimonYrievich <107398082+DimonYrievich@users.noreply.github.com>
2023-03-10 17:58:39 +0530

    Add files via upload

--- NewsPortal/sign/views.py
+++ NewsPortal/sign/views.py
@@ -10,19 +10,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
-class IndexView(LoginRequiredMixin, TemplateView):                              #ПРЕДСТАВЛЕНИЕ
+class IndexView(LoginRequiredMixin, TemplateView):    #ПРЕДСТАВЛЕНИЕ для отображения информации авторизованным пользователям и определения, состоят они в группе authors или нет
     template_name = 'news/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_not_author'] = not self.request.user.groups.filter(name='authors').exists()
         return context
-
-
-class BaseRegisterView(CreateView):
-    model = User
-    form_class = BaseRegisterForm
-    success_url = '/'
 
 
 @login_required                                     # Пишем функцию-представление для добавления пользователя в группу authors
@@ -33,6 +27,11 @@ def upgrade_me(request):
         authors_group.user_set.add(user)
     return redirect('/')
 
+
+class BaseRegisterView(CreateView):
+    model = User
+    form_class = BaseRegisterForm
+    success_url = '/'
 
 #class MyView(PermissionRequiredMixin, CreateView):
 #    permission_required = ('<app>.<action>_<model>',
